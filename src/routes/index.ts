41edf51a3d032cc369ac323ed1e727809a473939
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-09-08 20:24:45 -0300

    AT-2 feat: implementar crud de estações

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import helloRoutes from '../routes/helloRoutes'
+import estacaoRoutes from './estacaoRoutes'
 
 const router = Router();
 
 router.use('/hello', helloRoutes)
+router.use('/estacao', estacaoRoutes)
 
 export default router;
