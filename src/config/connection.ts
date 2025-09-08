0a6127815540387780419c906215a31199e68da1
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-09-08 20:24:45 -0300

    AT-2 feat: implementar crud de estações

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
+import Estacao from '../models/Estacao';
 
 dotenv.config();
 
@@ -9,8 +10,8 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD, // mudar senha
   host: process.env.DB_HOST, // colocar domínio
   port: parseInt(process.env.DB_PORT), // colocar porta
-  dialect: 'mysql', // colocar o banco de dados utilizado
-  models: [],  // Adicionar os modelos a serem trabalhados aqui
+  dialect: 'postgres', // colocar o banco de dados utilizado
+  models: [Estacao],  // Adicionar os modelos a serem trabalhados aqui
 });
 
 export default sequelize;
