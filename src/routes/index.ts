28db69225e0c8d6a39b40da97e5c429b015f7589
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-09-09 10:50:35 -0300

    AT-18 refactor: deletar hello world

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import helloRoutes from '../routes/helloRoutes'
 import usuarioRoutes from '../routes/usuarioRoutes'
 
 const router = Router();
 
-router.use('/hello', helloRoutes)
 router.use('/usuario', usuarioRoutes)
 
 export default router;
