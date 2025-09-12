52193cdb6ce1775725fada12bbd2303cf55a2b31
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-09-12 08:40:56 -0300

    AT-8 feat: adicionar crud tipo_parametro

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 import Estacao from '../models/Estacao';
+import TipoParametro from '../models/TipoParametro';
+import EstacaoTipoParametro from '../models/EstacaoTipoParametro';
+import ValorCapturado from '../models/ValorCapturado';
 
 dotenv.config();
 
@@ -11,7 +14,7 @@ const sequelize = new Sequelize({
   host: process.env.DB_HOST, // colocar domínio
   port: parseInt(process.env.DB_PORT), // colocar porta
   dialect: 'postgres', // colocar o banco de dados utilizado
-  models: [Estacao],  // Adicionar os modelos a serem trabalhados aqui
+  models: [Estacao, TipoParametro, EstacaoTipoParametro, ValorCapturado],  // Adicionar os modelos a serem trabalhados aqui
 });
 
 export default sequelize;
---
+++ src/models/TipoParametro.ts
@@ -0,0 +1,68 @@
+import { Table, Column, Model, DataType, BelongsToMany } from 'sequelize-typescript';
+import Estacao from './Estacao';
+import EstacaoTipoParametro from './EstacaoTipoParametro';
+
+@Table({
+    tableName: 'tipo_parametro',
+    timestamps: false
+})
+export default class TipoParametro extends Model {
+
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    })
+    pk!: number;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    json_id!: string | null;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    nome!: string | null;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    tipo!: string | null;
+
+    @Column({
+        type: DataType.DECIMAL(8, 4),
+        allowNull: true
+    })
+    offset!: number | null;
+
+    @Column({
+        type: DataType.DECIMAL(8, 4),
+        allowNull: true
+    })
+    fator!: number | null;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    polinomio!: string | null;
+
+    @Column({
+        type: DataType.STRING(20),
+        allowNull: true
+    })
+    unidade!: string | null;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: true
+    })
+    alarme!: number | null;
+
+    @BelongsToMany(() => Estacao, () => EstacaoTipoParametro, 'tipo_parametro_pk', 'estacao_est_pk')
+    estacoes!: Estacao[];
+}
