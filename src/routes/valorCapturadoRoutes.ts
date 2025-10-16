c2a1a50f5d0d4b7a1b20ba97adf015292bfacc25
Arthur Johannes <arthur.peres@fatec.sp.gov.br>
2025-10-16 03:23:05 -0300

    AT-46 feat: adicionado filtro de relatorio

--- src/routes/valorCapturadoRoutes.ts
+++ src/routes/valorCapturadoRoutes.ts
@@ -5,10 +5,11 @@ const routes = Router();
 
 routes.post('/', valorCapturadoController.save);       // Criar valor capturado
 routes.get('/', valorCapturadoController.findAll);     // Listar todos os valores
-routes.get('/:pk', valorCapturadoController.findById); // Buscar valor por ID
 routes.get('/parametro/:parametro_pk', valorCapturadoController.findByParametro); // Buscar valores por parâmetro
 routes.get('/estacao/:estacao_id', valorCapturadoController.findByEstacao); // Buscar valores por estação
 routes.get('/filtros/periodo', valorCapturadoController.findByDateRange); // Buscar valores por período e filtros
+routes.get('/relatorio/', valorCapturadoController.findByFilterReport);     // Listar todos os valores como relatório
+routes.get('/:pk', valorCapturadoController.findById); // Buscar valor por ID
 routes.put('/:pk', valorCapturadoController.update);   // Atualizar valor
 routes.delete('/:pk', valorCapturadoController.delete); // Deletar valor
 
